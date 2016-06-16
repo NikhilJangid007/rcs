047d40ddcd96aaa9a47427d9cf2fd0cca3bf9076
NikhilJangid007 <jangidnik@gmail.com>
2016-06-16 20:58:21 +0530

    Create p3.php

---
+++ p3.php
@@ -0,0 +1,100 @@
+{
+    "css": {
+        "font": [
+        ],
+        "import": [
+        ],
+        "rule": {
+             "#footer": {
+                "background": "#121212;",
+                 "border-top": "1px solid blue;"
+            },
+            "#playlist-button": {
+                "background": "#DF013A;"
+            },
+            "#dj-button": {
+                "background": "#121212;",
+                "color": "#DF013A;"
+            },
+            ".left": {
+                "background": "#0A0A0A;"
+            },
+            "#woot .bottom, #woot.selected": {
+                "background": "background: #D02490;"
+            },
+            "#meh .bottom, #meh.selected": {
+                "background": "#FE2E2E;"
+            },
+            "#room-host, #room-name": {
+                "text-transform": "uppercase;",
+                "color": "yellow;"
+            },
+            ".app-header": {
+                "background": "#121212;",
+                "border-bottom": "2px solid blue;"
+            },
+            "#history-button": {
+                "background": "#5A071C;"
+            },
+            "#app-menu, #app-menu .list": {
+                "background": "#121212;"
+            },
+            "#footer-user .buttons": {
+                "background": "#05051F;"
+            },
+            "#playlist-menu": {
+                "background": "#0E121B;",
+                "border-right": "1px solid blue;"
+            },
+            "#playlist-menu .button": {
+               "background": "#673AB7;"
+            },
+            "#user-menu": {
+               "background": "#121212;",
+                "border-right": "1px solid blue;"
+            },
+            "#room-settings-menu": {
+               "background": "#0E121B;",
+               "border-right": "1px solid blue"
+            },
+            "#chat .emote": {
+                "border-left": "rgba(174, 69, 189, 1) 3px solid !important",
+                "background-color": "rgba(190, 28, 213, 0.22) !important"
+            },
+            ".message.from-3625731 .from": {
+                 "pointer-events": "none !important"
+             },
+            "#chat-messages .emote .icon": {
+                "left": "4px"
+            },
+            "#chat .emote .text": {
+                "font-style": "normal",
+                "font-weight": "bold",
+                "color": "#ac76ff"
+            },
+             "#chat .emote.from-3625731 .text": {
+                "font-style": "normal",
+                "font-weight": "bold",
+                "color": "#ac76ff"
+            }
+              
+        }
+
+    },
+    "images": {
+        "background": "http://i.imgur.com/u36VR4n.png",
+        "booth": "",
+    "icons":{
+         "host": "http://i.imgur.com/pFxec23.png",
+        "cohost": "http://i.imgur.com/JYOmwwC.png",
+        "manager": "http://i.imgur.com/6PljHM4.png",
+        "bouncer": "http://i.imgur.com/roNNzQ9.png",
+        "rdj": "http://i.imgur.com/J1LOn5W.png",
+      }
+    },
+    "rules": {
+        "allowAutorespond": "true",
+        "allowAutowoot": "true",
+        "allowAutojoin": "true"
+    }
+}
